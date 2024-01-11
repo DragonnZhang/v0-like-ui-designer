73dc83152558b57e1b8cc2009d4bfd43afb543e3
DragonnZhang <731557579@qq.com>
2024-01-11 14:59:22 +0800

    chore: changed information

--- server/api/generateNewWebsite.post.ts
+++ server/api/generateNewWebsite.post.ts
@@ -5,7 +5,7 @@ const config = useRuntimeConfig()
 export default defineEventHandler(async (event) => {
   const { userPrompt } = await readBody(event)
 
-  console.log(`Processing ${userPrompt}`)
+  console.log(`Processing prompt: ${userPrompt}`)
   try {
     const res = await generatePage(userPrompt)
     console.log('response', res)
