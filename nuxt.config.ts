34f23dcef4c9cbf825c553129394fcc827521e90
DragonnZhang <731557579@qq.com>
2024-01-11 15:59:57 +0800

    fix: change css directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
   devtools: { enabled: true },
   runtimeConfig: {
     openaiApiKey: '',
     qwenApiKey: '',
     baiduApiKey: '',
     baiduSecretKey: '',
-    streaming: true,
-    model: 'qwen'
+    streaming: false,
+    model: 'openai'
   },
-  css: ['~/assets/css/main.scss'],
+  css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
