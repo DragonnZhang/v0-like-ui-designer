e3a0084238e3bb60efa55cffde206bc1d0419320
DragonnZhang <731557579@qq.com>
2024-02-02 14:28:47 +0800

    feat: add system and human message

--- server/utils/naturalLanguageAnalysis.ts
+++ server/utils/naturalLanguageAnalysis.ts
@@ -1,4 +1,4 @@
-import { PromptTemplate } from '@langchain/core/prompts'
+import { ChatPromptTemplate, PromptTemplate } from '@langchain/core/prompts'
 import { getModelInstance } from './llmAccessService'
 import { StringOutputParser } from '@langchain/core/output_parsers'
 
@@ -7,27 +7,27 @@ const config = useRuntimeConfig()
 const model = getModelInstance()
 
 export const generateHTMLFromNaturalLanguage = async (userPrompt: string) => {
-  const promptTemplate = PromptTemplate.fromTemplate(
-    `
-    You are a web ui designer. Create a web page based on user input and is styled using Tailwind CSS.
-    The design should be modern and minimalistic, providing a user-friendly interface.
+  const systemTemplate = `
+  You are a web ui designer. Create a web page based on user input and is styled using Tailwind CSS.
+  The design should be modern and minimalistic, providing a user-friendly interface.
 
-    Here are something you need to pay attention:
-    1.Analysis of User Input: Begin by deciphering the business logic conveyed through user input. Then, design the page layout and features based on this logic.
-    2.Image Handling: For image elements, set the src attribute to '/placeholder.svg'. This ensures a consistent placeholder image is used where necessary.
-    3.SVG Integration: If an SVG is used, it should be contextually appropriate and serve as an icon reflective of its function. Ensure to embed the SVG directly into the DOM.
-    4.Design Aesthetic: Embrace a contemporary and minimalist design style. This should be reflected in the choice of colors, typography, spacing, and overall layout.
-    5.Functionality and Interactivity: The core purpose of the page is to display user data effectively. Incorporate basic interactive elements like buttons or links to enhance user engagement.
-    
-    Note:Please generate the HTML code directly without using Markdown code block format(do not start with \`\`\`html or end with \`\`\`).
-    
-    user input: """
-    {userPrompt}
-    """
-    `
-  )
+  Here are something you need to pay attention:
+  1.Analysis of User Input: Begin by deciphering the business logic conveyed through user input. Then, design the page layout and features based on this logic.
+  2.Image Handling: For image elements, set the src attribute to '/placeholder.svg'. This ensures a consistent placeholder image is used where necessary.
+  3.SVG Integration: If an SVG is used, it should be contextually appropriate and serve as an icon reflective of its function. Ensure to embed the SVG directly into the DOM.
+  4.Design Aesthetic: Embrace a contemporary and minimalist design style. This should be reflected in the choice of colors, typography, spacing, and overall layout.
+  5.Functionality and Interactivity: The core purpose of the page is to display user data effectively. Incorporate basic interactive elements like buttons or links to enhance user engagement.
+  
+  Note:Please generate the HTML code directly without using Markdown code block format(do not start with \`\`\`html or end with \`\`\`).
+  `
+  const humanTemplate = '{userPrompt}'
 
-  const chain = promptTemplate.pipe(model)
+  const chatPrompt = ChatPromptTemplate.fromMessages([
+    ['system', systemTemplate],
+    ['human', humanTemplate]
+  ])
+
+  const chain = chatPrompt.pipe(model)
   const parser = new StringOutputParser()
   const llmResult = config.public.streaming
     ? await chain.pipe(parser).stream({
